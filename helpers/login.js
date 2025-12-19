7d6b1157c82962c0ea3f1bed75dd701b3a2e8cce
licdanielmillan-cloud <lic.daniel.millan@gmail.com>
2025-12-19 12:48:18 -0600

    Add documentation comment to login helper

--- helpers/login.js
+++ helpers/login.js
@@ -1,3 +1,5 @@
+
+// Login helper used across multiple tests
 export async function login(page, username, password) {
   await page.getByLabel('Username').fill(username);
   await page.getByLabel('Password').fill(password);
